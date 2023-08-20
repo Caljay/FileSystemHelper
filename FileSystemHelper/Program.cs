4dafa56994e2431adacca7e36183287fbb6f2eac
Caljay <99057688+Caljay@users.noreply.github.com>
2023-08-20 13:31:04 -0700

    Add project files.

---
+++ FileSystemHelper/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+   
+    static void FileNameRemoverAndCopier()
+    {
+        /* 
+         
+        First need to collect some information on:
+        where the files are
+        where to copy the files after words removed
+        what characters or words should be removed
+    
+         */
+
+        Console.WriteLine("Enter your intial File Path");
+        string filePath = Console.ReadLine();
+
+        Console.WriteLine("Where should the completed files go?");
+        string CompleteFiles = Console.ReadLine();
+
+        Console.WriteLine("What characters or words would you like to remove?");
+        string keywordForDeletion = Console.ReadLine(); ;
+
+
+
+        string[] allFiles = Directory.GetFiles(filePath);
+        List<string> renamedFiles = new List<string>();
+
+        foreach (var item in allFiles)
+        {
+            Console.WriteLine(item + "\n");
+
+            var newitem = item.Replace(keywordForDeletion, string.Empty);
+            var finalItem = newitem.Replace(filePath, string.Empty);
+            renamedFiles.Add(finalItem);
+            Console.WriteLine(newitem + "\n");
+
+        }
+        for (int i = 0; i < renamedFiles.Count; i++)
+        {
+            File.Copy(allFiles[i], CompleteFiles + "\\" + renamedFiles[i]);
+        }
+
+
+        Console.ReadKey();
+    }
+    static void Main()
+    {
+      
+        FileNameRemoverAndCopier();  
+    }
+
+   
+}
